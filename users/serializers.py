8842fa478c3832943f0a13642a62a69cdfe31a63
Juan Sebastian Alvarez Eraso <juan_alvarez_12345@hotmail.com>
2020-03-22 17:07:55 -0500

    Added users module

---
+++ users/serializers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from rest_auth.models import TokenModel
+from rest_auth.registration.serializers import RegisterSerializer
+from rest_framework import serializers
+
+User = get_user_model()
+
+
+class CustomRegisterSerializer(RegisterSerializer):
+    first_name = serializers.CharField(required=False)
+    last_name = serializers.CharField(required=False)
+
+    def custom_signup(self, request, user):
+        user.first_name = self.validated_data.get('first_name', '')
+        user.last_name = self.validated_data.get('last_name', '')
+        user.save(update_fields=['first_name', 'last_name'])
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'get_full_name')
+
+
+class CustomTokenSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+
+    class Meta:
+        model = TokenModel
+        fields = ('key', 'user')
